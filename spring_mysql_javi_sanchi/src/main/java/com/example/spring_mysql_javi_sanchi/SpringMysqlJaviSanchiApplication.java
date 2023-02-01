b247d8cacfd52a6d5ab034cca602898bb6ec7894
Javier <javi.ssweb@gmail.com>
2023-02-01 18:52:19 +0100

    Initial commit

---
+++ spring_mysql_javi_sanchi/src/main/java/com/example/spring_mysql_javi_sanchi/SpringMysqlJaviSanchiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mysql_javi_sanchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlJaviSanchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlJaviSanchiApplication.class, args);
+	}
+
+}
---
+++ spring_mysql_javi_sanchi/src/test/java/com/example/spring_mysql_javi_sanchi/SpringMysqlJaviSanchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mysql_javi_sanchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlJaviSanchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
